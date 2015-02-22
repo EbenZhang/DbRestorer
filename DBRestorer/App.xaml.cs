e731792d2acaee776e5a68f106e62ba6a50cb027
EbenZhang <zhangyingneng@gmail.com>
2015-02-22 21:48:44 +0800

    ViewModelLocator

--- DBRestorer/App.xaml.cs
+++ DBRestorer/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using GalaSoft.MvvmLight.Threading;
 
 namespace DBRestorer
@@ -14,6 +8,10 @@ namespace DBRestorer
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            ViewModelLocator.BootStrap();
+        }
         protected override void OnStartup(StartupEventArgs e)
         {
             DispatcherHelper.Initialize();
---
+++ DBRestorer/ViewModelLocator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using DBRestorer.Domain;
+using DBRestorer.Model;
+
+namespace DBRestorer
+{
+    public class ViewModelLocator
+    {
+        private static IContainer _container;
+
+        public static void BootStrap()
+        {
+            var builder = new ContainerBuilder();
+
+            // Usually you're only interested in exposing the type
+            // via its interface:
+            builder.RegisterType<SqlServerUtil>().As<ISqlServerUtil>();
+            builder.RegisterType<MainWindowVm>().AsSelf();
+            builder.RegisterType<SqlInstancesVM>().AsSelf();
+            _container = builder.Build();
+        }
+        public MainWindowVm MainWindowVm
+        {
+            get { return _container.Resolve<MainWindowVm>(); }
+        }
+    }
+}
