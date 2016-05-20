549e1ab857a4877f205292b64d365919d490d339
EbenZhang <zhangyingneng@gmail.com>
2016-05-20 22:51:14 +0800

    Able to Config Plugin Download Path

---
+++ Plugin_DbRestorerConfig/PluginDownloadFolderSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows;
+using DBRestorer.Plugin.Interface;
+
+namespace Plugin_DbRestorerConfig
+{
+    [Export(typeof(IDbRestorerSettings))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class PluginDownloadFolderSettings : IDbRestorerSettings
+    {
+        public string Name => "Plugin Download Folder"; 
+        public void ShowSettings(Window ownerWindow)
+        {
+            var wnd = new PluginDownloadFolderSettingsView {Owner = ownerWindow};
+            wnd.ShowDialog();
+        }
+    }
+}
---
+++ Plugin_DbRestorerConfig/PluginDownloadFolderSettingsView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using DBRestorer.Domain;
+
+namespace Plugin_DbRestorerConfig
+{
+    /// <summary>
+    /// Interaction logic for PluginDownloadFolderSettingsView.xaml
+    /// </summary>
+    public partial class PluginDownloadFolderSettingsView 
+    {
+        readonly UserPreferencePersist _persist = new UserPreferencePersist();
+        public PluginDownloadFolderSettingsView()
+        {
+            InitializeComponent();
+            this.Loaded += PluginDownloadFolderSettingsView_Loaded;
+            var pref = _persist.LoadPreference();
+            txtDownloadFolder.Text = pref.PluginDownloadPath;
+        }
+
+        private void PluginDownloadFolderSettingsView_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            txtDownloadFolder.Focus();
+        }
+
+        private void PluginDownloadFolderSettingsView_OnClosing(object sender, CancelEventArgs e)
+        {
+            var pref = _persist.LoadPreference();
+            pref.PluginDownloadPath = txtDownloadFolder.Text;
+            _persist.SavePreference(pref);
+        }
+    }
+}
