d608b28b99307652f463a8a35df205c3aeb2c902
EbenZhang <zhangyingneng@gmail.com>
2017-09-10 21:11:17 +0800

    Able to publish the dbrestorer

---
+++ ReleaseDbRestorer.ps1
@@ -0,0 +1,19 @@
+$ErrorActionPreference = 'Stop'
+$projectPath = Resolve-Path "./DBRestorer/DBRestorer.csproj"
+$csproj = [xml](Get-Content $projectPath)
+$propertyGroup = $csproj.Project.PropertyGroup[0]
+
+$thisRevision = [long]$propertyGroup.ApplicationRevision
+$previousVersion = $propertyGroup.ApplicationVersion.Replace("%2a", $thisRevision - 1)
+$folder = "DBRestorer_" + $previousVersion.Replace('.', '_')
+$previousVersionFolder = join-path './dist/Application Files/' $folder
+
+if(Test-Path $previousVersionFolder) {
+    Remove-Item $previousVersionFolder -Force -Recurse
+}
+Remove-Item './dist/*.nupkg' -Force -Recurse
+
+$propertyGroup.ApplicationRevision = ($thisRevision + 1).ToString()
+write-host $propertyGroup.ApplicationRevision
+$csproj.Save($projectPath)
+
